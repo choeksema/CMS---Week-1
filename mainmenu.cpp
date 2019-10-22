5cbeacaf6cc6e74c9e2627fddae14bbacac8691c
Gregory Huras <30447480+gr8gregory@users.noreply.github.com>
2019-10-22 15:49:01 -0400

    Update mainmenu.cpp

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -8,9 +8,16 @@
 
 #include <stdio.h>
 #include "menu.h"
+#include "message.h"
+#include "Queues.h"
+#include "sound.h"
 
 int main(void) {
 
+	Menu();
+}
+
+int Menu(void) {
 	char c;
 
 	char userIn[2] = { '\0' };
@@ -19,41 +26,43 @@ int main(void) {
 	while (error == 1) {
 		printf("CMS Main Menu\n**********************************************************\n");
 
-		printf("Sound (S)\nPhonebook (P)\nSettings (X)\nDebug menu (D)\n");
+		printf("Sound (1)\nPhonebook (2)\nSettings (3)\nDebug menu (4)\n");
 		scanf_s("%c", userIn, 2);
 
 		error = 0;
 
 		switch (userIn[0]) {
-			case ('s'):
-			case ('S'):
-				while ((c = getchar()) != '\n' && (c != EOF)) {}				//flushes input 
-				sound();
-				// printf("Sound Menu\n**********************************************************\n");
-				break;
-			case('p'):
-			case('P'):
-				printf("Phonebook\n**********************************************************\n");
-				break;
-			case('X'):
-			case('x'):
-				printf("Settings Menu\n**********************************************************\n");
-				break;
-			case('D'):
-			case('d'):
-				printf("Debug Menu\n**********************************************************\n");
-				break;
-			case('\n'):
-				error = 1;
-				break;
-			default:
-				printf("Invalid input\n\n");
-				error = 1;
-				while ((c = getchar()) != '\n' && (c != EOF)) {}				//flushes input 
-				break;
+		case ('1'):
+			while ((c = getchar()) != '\n' && (c != EOF)) {}				//flushes input 
+			sound();
+			//printf("Sound Menu\n**********************************************************\n");
+			break;
+		case('2'):
+			while ((c = getchar()) != '\n' && (c != EOF)) {}
+			//printf("Phonebook\n**********************************************************\n");
+			phoneBook();
+			break;
+		case('3'):
+			while ((c = getchar()) != '\n' && (c != EOF)) {}
+			//printf("Settings Menu\n**********************************************************\n");
+			settings();
+			break;
+		case('4'):
+			while ((c = getchar()) != '\n' && (c != EOF)) {}
+			//printf("Debug Menu\n**********************************************************\n");
+			debug();
+			break;
+		case('\n'):
+			while ((c = getchar()) != '\n' && (c != EOF)) {}
+			error = 1;
+			break;
+		default:
+			printf("Invalid input\n\n");
+			error = 1;
+			while ((c = getchar()) != '\n' && (c != EOF)) {}				//flushes input 
+			break;
 		}
-		
-	}
 
-	return(0);
+	}
+	return 0;
 }
